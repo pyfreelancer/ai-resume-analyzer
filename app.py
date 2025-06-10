091cac9e650e3332836e478191f9e0184b4df5f8
Areej Sajjad <areejsajjad44@gmail.com>
2025-06-11 04:48:38 +0500

    🔄 Updated app.py with spaCy fix and improvements

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from spacy.cli import download
 # --- Load small spaCy model (en_core_web_sm) ---
 try:
     nlp = spacy.load("en_core_web_sm")
+
 except OSError:
     download("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
